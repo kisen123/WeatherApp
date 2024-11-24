e419185163e871f16756fa66666a050a34554243
Kristian Mølbach Lian <molbachlian@gmail.com>
2024-11-24 11:25:14 +0100

    Widget 1.0 with hardcoded values

--- src/modules/mainpage/live_weather/LiveWeather.tsx
+++ src/modules/mainpage/live_weather/LiveWeather.tsx
@@ -1,29 +1,65 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { useState } from 'react';
 import './LiveWeather.css';
+import LocationSearch from './LocationSearch';
 
-function LiveWeather() {
 
-    const [geoLocation, setGeoLocation] = useState('');
-    const [testInput, setTestInput] = useState('Some random input');
-
-    return (
-        <div className='weather-div'>
-            <input 
-                className="weather-inputs"
-                name="weather-location"
-                type="text"
-                onChange={event => setGeoLocation(event.target.value)}
-            />
-            <input 
-                className="weather-inputs"
-                name="test-input"
-                type="text" 
-            />
-            <p>The location is: {geoLocation}</p>
-        </div>
-            
-    );
+// API data goes here, test values for now
+interface WeatherData {
+    temperature: string;
+    humidity: string;
+    windSpeed: string;
+    location: string;
+    condition: string;
 }
 
-export default LiveWeather;
+
+//const [geoLocation, setGeoLocation] = useState('');
+//const [testInput, setTestInput] = useState('Some random input');
+
+const WeatherWidget: React.FC = () => {
+
+    // Test values weather data
+    const weatherData: WeatherData = {
+        temperature: "-1°C",
+        humidity: "40%",
+        windSpeed: "3 m/s",
+        location: "Trondheim, Heimdal",
+        condition: "Overskyet",
+    }
+
+
+
+    return (
+        <div className="weather-widget">
+            <div className="header">
+                <h1>Weather Widget</h1>
+            </div>
+            <div className="details">
+                <div>
+                <strong>Temperature:</strong> {weatherData.temperature}
+                </div>
+                <div>
+                <strong>Humidity:</strong> {weatherData.humidity}
+                </div>
+                <div>
+                <strong>Wind Speed:</strong> {weatherData.windSpeed}
+                </div>
+                <div>
+                <strong>Condition:</strong> {weatherData.condition}
+                </div>
+                
+                <h2 className="location-search-result">{weatherData.location}</h2>
+                <LocationSearch />
+            </div>
+
+
+        </div>
+
+
+        );
+    }
+
+
+export default WeatherWidget;
---
+++ src/modules/mainpage/live_weather/LocationSearch.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+//import axios from "axios";
+
+interface Location {
+    name: string;
+    country: string;
+    lat: number;
+    lon: number;
+    }
+
+
+const LocationSearch: React.FC = () => {
+    
+
+
+    return (
+        <div className="geolocation-search-div">Search bar goes here</div>
+    )
+}
+
+
+export default LocationSearch;
